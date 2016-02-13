23a83dc1a2ec30d6e254de0838f7f89193befec4
brad-williams <dynamic.bw@gmail.com>
2016-02-12 20:13:26 -0500

    Add basic index

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>PHP Example</title>
+<link rel="stylesheet" href="css/main.css" />
+</head>
+<body>
+<h1>PHP Example</h1>
+<p>Please take our survey below!</p>
+<form action="form.php">
+<p>Select your gender:<br />
+M<input type="radio" name="gender" value="male"><br />
+F<input type="radio" name="gender" value="female">
+</p>
+</form>
+</body>
+</html>
